711f0d05c2a0d36ca1709a6a8b4ed34d65d7d118
Mayya <73754515+mmukhina@users.noreply.github.com>
2025-03-24 19:56:04 +0300

    Update master_esp32.ino

--- v3/arena/master_esp32/master_esp32.ino
+++ v3/arena/master_esp32/master_esp32.ino
@@ -1,12 +1,12 @@
 #include <Wire.h>
 
-#define TCA9548A_ADDRESS  0x70  // Default I2C address of TCA9548A
-#define RESTART_BTN_PIN   4
-#define NUM_STATIONS      3
-#define NUM_TASKS         2
+#define TCA9548A_ADDRESS 0x70  // Default I2C address of TCA9548A
+#define RESTART_BTN_PIN 4
+#define NUM_STATIONS 3
+#define NUM_TASKS 2
 
-char red_robot_id[8] = "14393707";
-char blue_robot_id[8] = "123";
+char red_robot_id[9] = "14393707";
+char blue_robot_id[9] = "14393707";
 
 int center_station_addres = 1;
 
@@ -16,7 +16,9 @@ int blue_task_count = 0;
 unsigned long game_start_time;
 const unsigned long duration = 5000;
 
-enum GameState { NONE, TASKS, FLAG};
+enum GameState { NONE,
+                 TASKS,
+                 FLAG };
 GameState game_state = NONE;
 
 // Define station states
@@ -31,8 +33,8 @@ struct Tasks {
 };
 
 Tasks all_tasks[NUM_TASKS] = {
-  {"679c8736ed61c22d6e28f9b2", 1, 'r', false, false},
-  {"679c8736ed61c22d6e28f9b2", 2, 'b', false, false},
+  { "679c8736ed61c22d6e28f9b2", 1, 'r', false, false },
+  { "679c8736ed61c22d6e28f9b2", 2, 'b', false, false },
 };
 
 struct Station {
@@ -41,36 +43,25 @@ struct Station {
 };
 
 Station all_stations[NUM_STATIONS] = {
-  {1, COLLECT},
-  {2, COLLECT},
-  {3, DROP},
+  { 1, COLLECT },
+  { 2, COLLECT },
+  { 3, DROP },
 };
 
 #pragma pack(1)
-struct send_dataPacket {
+struct Datasend_data {
   StationState state;
-  char robot_id[8];
+  char robot_id[9];
   char robot_color;
 };
 #pragma pack()
 
-send_dataPacket send_data;
+Datasend_data send_data = { COLLECT, "12345678", 'a' };  // Sample data to send
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   pinMode(RESTART_BTN_PIN, INPUT_PULLUP);
-}
-
-void sendMessage(uint8_t address, const char* message) {
-  Wire.beginTransmission(address);
-  Wire.write(message, strlen(message));
-}
-
-void start_tasks() {
-  for (uint8_t i = 1; i <= NUM_STATIONS; i++) {
-    sendMessage(i, "c_123_r");
-  }
 }
 
 void get_slave_send_data() {
@@ -89,14 +80,8 @@ void get_slave_send_data() {
   }
 }
 
-void send_off() {
-  for (uint8_t i = 1; i <= NUM_STATIONS; i++) {
-    sendMessage(i, "o");
-  }
-}
-
 void send_station_state() {
-  for (int i = 0; i = NUM_STATIONS; i++) {
+  for (int i = 0; i < NUM_STATIONS; i++) {
     int address = all_stations[i].address;
     StationState state = all_stations[i].state;
 
@@ -108,22 +93,22 @@ void send_station_state() {
       for (int j = 0; j < NUM_TASKS; j++) {
         if (all_tasks[j].station_address == address) {
           char robot_color = all_tasks[j].robot_color;
-          uint16_t robot_id = (robot_color == 'r') ? red_robot_id : blue_robot_id;
+          //char robot_id[9] = (robot_color == 'r') ? red_robot_id : blue_robot_id;
           send_data.state = state;
-          strcpy(send_data.robot_id, robot_id);
+          strcpy(send_data.robot_id, (robot_color == 'r') ? red_robot_id : blue_robot_id);
           send_data.robot_color = robot_color;
           break;
         }
       }
     }
+
     Wire.beginTransmission(address);
-    Wire.write((uint8_t*)&send_data, sizeof(send_data));  // Convert struct to bytes and send
+    Wire.write((uint8_t*)&send_data, sizeof(send_data));  // Send struct as bytes
     Wire.endTransmission();
   }
 }
 
 void get_station_info() {
-
 }
 
 void loop() {
@@ -147,5 +132,5 @@ void loop() {
     }
   }
 
-  delay(500); // Reduce CPU load
+  delay(500);  // Reduce CPU load
 }
